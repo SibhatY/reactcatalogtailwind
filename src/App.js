5d0deddf1573bd1e25454026953686bd39fe2773
ybasha24 <ybasha2020@gmail.com>
2023-04-09 18:49:09 -0500

    idk

--- src/App.js
+++ src/App.js
@@ -12,43 +12,108 @@ export const App = () => {
   const [ProductsCategory, setProductsCategory] = useState(Products);
   const [query, setQuery] = useState('');
   // var ProductsCategory = Products;
+  const [cart, setCart] = useState({});
+
+
+  // const render_products = (ProductsCategory) => {
+
+  //   return <div className='category-section fixed'>
+	//   {console.log("Step 3 : in render_products ")}
+  //     <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
+
+  //     <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{ maxHeight: '800px', overflowY: 'scroll' }}>
+  //       {/* Loop Products */}
+  //       {ProductsCategory.map((product, index) => (
+  //         <div key={index} className="group relative shadow-lg" >
+  //           <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
+  //             <img
+  //               alt = "player"
+  //               src= {product.image}
+  //               className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+  //             />
+  //           </div>
+  //           <div className="flex justify-between p-3">
+  //             <div>
+  //               <h3 className="text-sm text-gray-700">
+  //                 <a href={product.href}>
+  //                   <span aria-hidden="true" className="absolute inset-0" />
+  //                   <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
+  //                 </a>
+  //                 <p>Tag - {product.category}</p>
+  //               </h3>
+  //               <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
+  //             </div>
+  //             <p className="text-sm font-medium text-green-600">${product.price}</p>
+  //           </div>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   </div>
+  // }
 
 
   const render_products = (ProductsCategory) => {
-
-    return <div className='category-section fixed'>
-	  {console.log("Step 3 : in render_products ")}
-      <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
-
-      <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{ maxHeight: '800px', overflowY: 'scroll' }}>
-        {/* Loop Products */}
-        {ProductsCategory.map((product, index) => (
-          <div key={index} className="group relative shadow-lg" >
-            <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
-              <img
-                alt = "player"
-                src= {product.image}
-                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-              />
-            </div>
-            <div className="flex justify-between p-3">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href={product.href}>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
-                  </a>
-                  <p>Tag - {product.category}</p>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
+    return (
+      <div className="category-section fixed">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">
+          Products ({ProductsCategory.length})
+        </h2>
+        <div
+          className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10"
+          style={{ maxHeight: "800px", overflowY: "scroll" }}
+        >
+          {ProductsCategory.map((product, index) => (
+            <div key={index} className="group relative shadow-lg">
+              <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
+                <img
+                  alt="player"
+                  src={product.image}
+                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                />
               </div>
-              <p className="text-sm font-medium text-green-600">${product.price}</p>
-            </div>
+              <div className="flex justify-between p-3">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a href={product.href}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      <span style={{ fontSize: "16px", fontWeight: "600" }}>
+                        {product.title}
+                      </span>
+                    </a>
+                    <p>Tag - {product.category}</p>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">
+                    Rating: {product.rating.rate}
+                  </p>
+                </div>
+                <div>
+                  <p className="text-sm font-medium text-green-600">
+                    ${product.price}
+                  </p>
+                  <div className="flex mt-2">
+                    <button
+                      className="bg-green-500 text-white rounded-l py-1 px-2"
+                      onClick={() => handleAddToCart(product)}
+                    >
+                      +
+                    </button>
+                    <span className="border border-green-500 bg-white py-1 px-2">
+                      {cart[product.id] ? cart[product.id].quantity : 0}
+                    </span>
+                    <button
+                      className="bg-red-500 text-white rounded-r py-1 px-2"
+                      onClick={() => handleRemoveFromCart(product)}
+                    >
+                      -
+                    </button>
+                  </div>
+                </div>
+              </div>
+              </div>
+          ))}
           </div>
-        ))}
-      </div>
-    </div>
-  }
+          </div>
+    )}
 
   
   function handleClick(tag){
@@ -62,7 +127,33 @@ export const App = () => {
     // ProductsCategory = filtered;
     console.log("Step 5 : ", Products.length, ProductsCategory.length);
     }
-  }
+  };
+
+  const handleAddToCart = (product) => {
+    setCart((prevCart) => {
+      const newCart = { ...prevCart };
+      if (product.id in newCart) {
+        newCart[product.id].quantity += 1;
+      } else {
+        newCart[product.id] = { ...product, quantity: 1 };
+      }
+      return newCart;
+    });
+  };
+  
+  const handleRemoveFromCart = (product) => {
+    setCart((prevCart) => {
+      const newCart = { ...prevCart };
+      if (product.id in newCart) {
+        if (newCart[product.id].quantity > 1) {
+          newCart[product.id].quantity -= 1;
+        } else {
+          delete newCart[product.id];
+        }
+      }
+      return newCart;
+    });
+  };
 
   
   const handleChange = (e) => {
